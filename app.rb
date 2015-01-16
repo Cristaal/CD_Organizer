cc0f34fb4cf03563a6d430f3e5283a92c614ab5a
Crystal Contreras-Grossman & Monty Rohde <student@epicodus.com>
2015-01-15 16:38:19 -0800

    include a working search function

--- app.rb
+++ app.rb
@@ -6,29 +6,31 @@ require ('./lib/artist')
 require ('./lib/album_name')
 require ('pry')
 
+Cd.clear()
+
 get ('/') do
   erb(:form)
 end
 
 post('/submit') do
-  artist = Artist.new(params.fetch('artist'))
+  artist = Artist.new({:name => params.fetch('artist')})
   artist.save()
-  album = Album.new(params.fetch('album'))
+  album = Album.new({:album_name => params.fetch('album')})
   album.save()
-  cd = Cd.new(artist, album)
+  cd = Cd.new({:artist => artist, :album_name => album})
   cd.save()
   redirect ('/')
 end
 
 get('/artist_search') do
-  @cd = Cd.search_artist("search_artist")
+  @cd = Cd.search_artist(params.fetch("artist_search"))
   @artist = (@cd.artist().name())
   @album = (@cd.album_name().album_name())
   erb(:organizer)
 end
 
 get('/album_search') do
-   @cd = Cd.search_album("album_search")
+   @cd = Cd.search_album(params.fetch("album_search"))
    @artist = (@cd.artist().name())
    @album = (@cd.album_name().album_name())
    erb(:organizer)
